e3b8c9a52e31688ff97b6bcf9ad0f06d3b574401
@networkpankaj <@networkpankaj>
2024-05-28 11:40:27 +0000

    initialize

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express')
+
+const app = express()
+
+const server = require('http').Server(app);
+
+const { v4 : uuidv4 } = require('uuid');
+
+app.set('view engine', 'ejs');
+
+app.use(express.static('public'))
+
+
+
+app.get('/', (req, res) => {
+    // res.status(200).send('Hello world');
+    // res.render('room');
+    res.redirect(`/${uuidv4()}`);
+})
+
+app.get('/:room', (req, res) => {
+    res.render('room', { roomId: req.params.room })
+})
+
+
+server.listen(3030);
